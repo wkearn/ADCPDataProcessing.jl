54a36a7baeaa34872dd5ba8889e2b309a86078ef
Will Kearney <wkearn@gmail.com>
2017-03-23 18:44:45 -0400

    Calibration -> CalibrationDeployment
    This is ugly

--- src/calibrations.jl
+++ src/calibrations.jl
@@ -1,17 +1,17 @@
-export Calibration, parse_cals
+export CalibrationDeployment, parse_cals
 
 #####################################################
 # Definition of Calibration type and loading
 # calibration definitions from METADATA.json
 
-type Calibration
+type CalibrationDeployment
     deployment::Deployment
     cs::CrossSection
     startDate::DateTime
     endDate::DateTime
 end
 
-function Base.show(io::IO,cal::Calibration)
+function Base.show(io::IO,cal::CalibrationDeployment)
     println(io,"Calibration")
     println(io,"------------")
     println(io,"Start time: ",cal.startDate)
@@ -30,23 +30,23 @@ function parse_cals{C}(creek::Creek{C})
         sD = DateTime(cal["startDate"])
         eD = DateTime(cal["endDate"])
         dep_match = findfirst(hex.(hs,16),dep)
-        push!(cals,Calibration(deps[dep_match],cs,sD,eD))
+        push!(cals,CalibrationDeployment(deps[dep_match],cs,sD,eD))
     end
     cals
 end
 
-function Base.hash(x::Calibration,h::UInt)
+function Base.hash(x::CalibrationDeployment,h::UInt)
     h = hash(x.deployment,h)
     h = hash(x.startDate,h)
     h = hash(x.endDate,h)
 end
 
-Base.:(==)(c1::Calibration,c2::Calibration) = hash(c1)==hash(c2)
+Base.:(==)(c1::CalibrationDeployment,c2::CalibrationDeployment) = hash(c1)==hash(c2)
 
 #####################################################
 # Loading calibration data
 
-function load_data(cal::Calibration)
+function load_data(cal::CalibrationDeployment)
     # Load ADCP data and convert to discharges
     ad = load_data(cal.deployment)
     cs = load_data(cal.cs)
@@ -57,5 +57,5 @@ function load_data(cal::Calibration)
                         "calibrations",
                         hex(hash(cal),16))
     D = readtable(joinpath(data_dir,"discharge_calibrations.csv"))
-    CalibrationData(cal,DateTime(D[:DateTime]),D[:SP_Q],dd)
+    Calibration(cal,DateTime(D[:DateTime]),D[:SP_Q],dd)
 end
